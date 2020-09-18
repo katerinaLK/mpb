45af0ce2069e197ca567f6d4c033f8565be149be
L <63997626+owlatarms@users.noreply.github.com>
2020-09-18 14:23:20 +0300

    Загрузка проектов

---
+++ #8.2_PWM_[Variable_Frequency]/Source/main.h
@@ -0,0 +1,20 @@
+﻿/******************************************************************************
+ * @File: main.h
+ * @Author: Milandr, L.
+ * @Project: #8.2_PWM_[Variable_Frequency]
+ * @Version: 1.0.1
+ * @Compiler: ARM Compiler V5.06 (build 750)
+ * @Microcontroller: К1986ВЕ92QI
+ * @Device: Отладочная плата «МилКиТЭС»
+ * @Date: 02.04.2020
+ * @Purpose: Основной модуль
+ ******************************************************************************/
+
+// Условная компиляция
+#ifndef __MAIN_H
+#define __MAIN_H
+
+// Подключение заголовков
+#include "link.h"
+
+#endif
